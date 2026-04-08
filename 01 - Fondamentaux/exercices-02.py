da8e97157f5466bdbe3ed40cec77becfe58eff9b
Steve Vandenbossche <steve@ecomdesign.be>
2026-04-08 14:35:05 +0200

    update exercices-02 + add pdf

--- 01 - Fondamentaux/exercices-02.py
+++ 01 - Fondamentaux/exercices-02.py
@@ -1,8 +1,9 @@
 prenom = input("Quel est votre prenom ? ")
 
-salutation = print(f"Bienvenue {prenom}")
+salutation = f"Bienvenue {prenom}"
+print(salutation)
 
 age = input("Quel est votre age ? ")
 age = int(age)
 
-age_prochain = print(f"Bienvenue {prenom}, vous avez {age} ans et l'année prochaine vous aurez {age+1} ans")
+print(f"{salutation}, vous avez {age} ans et l'année prochaine vous aurez {age+1} ans")
