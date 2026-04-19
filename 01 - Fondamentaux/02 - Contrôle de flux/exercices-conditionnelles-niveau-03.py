2a73e98d1669eefae558f8a61aff3cb0f6be7e33
Steve Vandenbossche <steve@ecomdesign.be>
2026-04-20 00:09:25 +0200

    update exercices-conditionnelles-niveau-03

--- 01 - Fondamentaux/02 - Contrôle de flux/exercices-conditionnelles-niveau-03.py
+++ 01 - Fondamentaux/02 - Contrôle de flux/exercices-conditionnelles-niveau-03.py
@@ -6,56 +6,54 @@
 # 5% entre 20 et 50 || 10% entre 51 et 100 || 15% pour les montants supérieurs à 100
 
 type_cafe = input("Type de café (espresso, latte, cappuccino) : ")
-match type_cafe :
-    case t if t == "espresso" : 
-        cafe = "espresso"
-        prix_cafe = 5    
-    case t if t == "latte" : 
-        cafe = "latte"
+
+match type_cafe:
+    case "espresso":
+        prix_cafe = 5
+    case "latte":
         prix_cafe = 10
-    case t if t == "cappuccino" :
-        cafe = "cappuccino"
+    case "cappuccino":
         prix_cafe = 20
     case _:
-        cafe = "Type de café inconnu"
+        print("Type de café invalide")
         prix_cafe = 0
 
 taille_cafe = input("Taille de café (petit, moyen, grand) : ")
-match taille_cafe :
-    case t if t == "petit" :
-        taille = "petit"
+
+match taille_cafe:
+    case "petit":
         prix_taille = 5
-    case t if t == "moyen" :
-        taille = "moyen"
+    case "moyen":
         prix_taille = 10
-    case t if t == "grand" :
-        taille = "grand"
+    case "grand":
         prix_taille = 20
-    case _ : 
-        taille = "Taille de café inconnue"
+    case _:
+        print("Taille invalide")
         prix_taille = 0
 
 nbre_cafe = int(input("Nombre de café : "))
 
 cout_total = (prix_cafe + prix_taille) * nbre_cafe
 
-print(f'Voici le détail de votre commande : café: {cafe} / taille: {taille} / nombre: {nbre_cafe}')
-print(f'Total : {cout_total} €')
+print("\nCommande :", type_cafe, "/", taille_cafe, "/ x", nbre_cafe)
+print("Total avant réduction :", cout_total, "€")
 
-if cout_total > 100 :
-    reduction = int((cout_total * 15) / 100)
-    total_reduc = int(cout_total - reduction)
-    print(f'Total avec réduction: {cout_total} - {reduction} = {total_reduc} €')
-elif cout_total >= 51 :
-    reduction = int((cout_total * 10) / 100)
-    total_reduc = int(cout_total - reduction)
-    print(f'Total avec réduction: {cout_total} - {reduction} = {total_reduc} €')
-elif cout_total >= 20 :
-    reduction = int((cout_total * 5) / 100)
-    total_reduc = int(cout_total - reduction)
-    print(f'Total avec réduction: {cout_total} - {reduction} = {total_reduc} €')
+# Réduction
+reduction = 0
+
+if cout_total > 100:
+    reduction = 0.15
+elif cout_total >= 51:
+    reduction = 0.10
+elif cout_total >= 20:
+    reduction = 0.05
+
+total_final = cout_total - (cout_total * reduction)
+
+if reduction > 0:
+    print("Réduction :", reduction * 100, "%")
+    print("Total après réduction :", total_final, "€")
 else:
-    print("pas de réduction")
-
+    print("Pas de réduction")
 
 
